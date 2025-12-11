07bd8f171f5d496cf3b9768cf600364af0163b92
BismaBRJ <bismabrj@gmail.com>
2025-12-11 16:58:27 +0700

    Pretty much finished migrating all "inner" code within main.js into tidy.js, but still gotta fix some regex

--- main.js
+++ main.js
@@ -7,19 +7,15 @@ function cleanup_par() {
     document.getElementById("box_result").textContent = clean_par;
 }
 
+// using functions defined in tidy.js
 function get_clean_par(source_par) {
     let no_nbsp = del_nbsp(source_par);
     let single_br = dedup_br(no_nbsp);
-
-    // let no_p_br = single_br.replaceAll(/<p>(\s*?)<br>(\s*?)/, "<p>$1");
+    let no_br_near_p = del_br_near_p(single_br);
+    let plain_p = simplify_p(no_br_near_p);
+    let all_p = br_to_close_p(plain_p);
     
-    // let no_p_br_p = no_p_br.replaceAll();
-    
-    // let plain_p = single_br.replaceAll(/<p [^>]*?>/, "<p>");
-    
-    // let all_p = no_p_br.replaceAll(/(\s+)<br>(\s+)/, "$1</p>\n<p>$2");
-    
-    let result = single_br;
+    let result = all_p;
     return result;
 }
 
--- tidy.js
+++ tidy.js
@@ -21,3 +21,38 @@ function dedup_br(par) {
     return new_par;
 }
 
+// delete <br> tags near the beginning/end of <p> tags
+function del_br_near_p(par) {
+    let new_par = par;
+    new_par = new_par.replaceAll(
+        /<p>(\s*?)<br>(\s*?)/,
+        "<p>$1"
+    );
+    new_par = new_par.replaceAll(
+        /(\s*?)<br>(\s*?)</p>/,
+        "</p>"
+    );
+    return new_par;
+}
+
+// remove all attributes of <p> tags
+function simplify_p(par) {
+    let new_par = par;
+    new_par = new_par.replaceAll(
+        /<p [^>]*?>/g,
+        "<p>"
+    );
+    return new_par;
+}
+
+// turn <br> tags into <p> tags
+// so that paragraphs are actually separated by <p> tags rather than <br> tags
+function br_to_close_p(par) {
+    let new_par = par;
+    new_par = new_par.replaceAll(
+        /(\s+)<br>(\s+)/g,
+        "$1</p>\n<p>$2"
+    );
+    return new_par;
+}
+
