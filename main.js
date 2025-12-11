95e9711b1979c39aade37eba6eb95c17217d5eb4
BismaBRJ <bismabrj@gmail.com>
2025-12-11 23:42:25 +0700

    Replace let with const in main.js (but not in tidy_funcs.js)

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ document.getElementById("btn_cleanup")
     .addEventListener("click", cleanup_par);
 
 function cleanup_par() {
-    let source_par = document.getElementById("box_input").value;
-    let clean_par = get_clean_par(source_par);
+    const source_par = document.getElementById("box_input").value;
+    const clean_par = get_clean_par(source_par);
     document.getElementById("box_result").textContent = clean_par;
 }
 
 // using functions defined in tidy.js
 function get_clean_par(source_par) {
-    let no_nbsp = del_nbsp(source_par);
-    let single_br = dedup_br(no_nbsp);
-    let no_br_near_p = del_br_near_p(single_br);
-    let plain_p = simplify_p(no_br_near_p);
-    let all_p = br_to_close_p(plain_p);
-    let left_p = del_spaces_before_p(all_p);
+    const no_nbsp = del_nbsp(source_par);
+    const single_br = dedup_br(no_nbsp);
+    const no_br_near_p = del_br_near_p(single_br);
+    const plain_p = simplify_p(no_br_near_p);
+    const all_p = br_to_close_p(plain_p);
+    const left_p = del_spaces_before_p(all_p);
         
-    let result = left_p;
+    const result = left_p;
     return result;
 }
 
