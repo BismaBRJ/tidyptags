3d8901ba24721b09c1fe9a7009b6f4d581692b74
BismaBRJ <bismabrj@gmail.com>
2025-12-12 00:10:13 +0700

    Added a debug mode, toggleable via a checkbox. So far it uses the alert function at every step of the way lol, and also warns whenever the result is empty

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+let debug_mode = false;
+const print = alert;
+
+document.getElementById("chk_debug")
+    .addEventListener(
+        "change",
+        (event) => set_debug(event)
+    );
+
+function set_debug(event) {
+    const mode = event.target.checked;
+    debug_mode = mode;
+}
+
 document.getElementById("btn_cleanup")
     .addEventListener("click", cleanup_par);
 
@@ -9,13 +23,31 @@ function cleanup_par() {
 
 // using functions defined in tidy.js
 function get_clean_par(source_par) {
+    if (debug_mode) { print("Removing nbsp"); }
     const no_nbsp = del_nbsp(source_par);
+    if (debug_mode && no_nbsp === "") { print("Empty result"); }
+
+    if (debug_mode) { print("Deduplicating <br> tags"); }
     const single_br = dedup_br(no_nbsp);
+    if (debug_mode && single_br === "") { print("Empty result"); }
+
+    if (debug_mode) { print("Deleting <br> tags near <p> tags"); }
     const no_br_near_p = del_br_near_p(single_br);
+    if (debug_mode && no_br_near_p === "") { print("Empty result"); }
+    
+    if (debug_mode) { print("Removing <p> tag attributes"); }
     const plain_p = simplify_p(no_br_near_p);
+    if (debug_mode && plain_p === "") { print("Empty result"); }
+
+    if (debug_mode) { print("Splitting paragraphs at <br> tags"); }
     const all_p = br_to_close_p(plain_p);
+    if (debug_mode && all_p === "") { print("Empty result"); }
+
+    if (debug_mode) { print("Dedenting <p> tags"); }
     const left_p = del_spaces_before_p(all_p);
-        
+    if (debug_mode && left_p === "") { print("Empty result"); }
+
+    if (debug_mode) { print("Finishing cleaning..."); }
     const result = left_p;
     return result;
 }
